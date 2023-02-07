eaf1804d876beca200c515643ae42e3b9bceaec8
92sagarchougule <92chougulesagar@gmail.com>
2023-02-07 14:51:29 +0530

    Map

--- agritech-app/src/App.js
+++ agritech-app/src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  Map, Layers, layer
+} from "react-openlayers";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<>
+  
+  <Map view={{center:[0,0], zoom:2}}>
+
+      <Layers>
+
+        <layer.Tile>
+
+        </layer.Tile>
+      </Layers>
+
+    </Map>
+    
+    <h1> Hello World</h1>
+
+  </>
+    
+
   );
 }
 
